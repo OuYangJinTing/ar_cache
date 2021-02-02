c4d7fe0566024966c9ed8949f035a73b2f136cdd
OuYangJinTing <2729877005qq@gmail.com>
2021-02-03 02:07:08 +0800

    Fix find and find_by skip statement cache condition

--- lib/ar_cache/active_record/querying.rb
+++ lib/ar_cache/active_record/querying.rb
@@ -6,11 +6,11 @@ module ArCache
       delegate :skip_ar_cache, to: :all
 
       def find(*ids)
-        defined?(@ar_cache_model) && ar_cache_model.disabled? ? super : all.find(*ids) # force skip statement cache
+        defined?(@ar_cache_model) && ar_cache_model.enabled? ? all.find(*ids) : super # force skip statement cache
       end
 
       def find_by(*args)
-        defined?(@ar_cache_model) && ar_cache_model.disabled? ? super : all.find_by(*args) # force skip statement cache
+        defined?(@ar_cache_model) && ar_cache_model.enabled? ? all.find_by(*args) : super # force skip statement cache
       end
     end
   end
