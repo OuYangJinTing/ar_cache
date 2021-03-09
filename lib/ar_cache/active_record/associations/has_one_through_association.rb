ea3daec98ddc9a81e662a99760e4a55b365f2fa3
OuYangJinTing <2729877005qq@gmail.com>
2021-03-09 13:56:42 +0800

    Fix has_one through cache error

--- lib/ar_cache/active_record/associations/has_one_through_association.rb
+++ lib/ar_cache/active_record/associations/has_one_through_association.rb
@@ -17,20 +17,20 @@ module ArCache
           end
 
           # TODO: Should not instantiate AR
-          through_record = if reflection.scope
-                             owner.association(reflection.through_reflection.name).scope.merge(reflection.scope).first
-                           else
-                             owner.send(reflection.through_reflection.name)
-                           end
-          return super if through_record.is_a?(::ActiveRecord::Associations::CollectionProxy)
-          return nil if !through_record || through_record.destroyed?
+          through_record = owner.send(reflection.through_reflection.name)
+          through_record = through_record.first if through_record.is_a?(::ActiveRecord::Relation)
+          return nil if through_record.nil? || through_record.destroyed?
 
-          record = through_record.send(reflection.source_reflection.name)
-          record = record.first if record.is_a?(::ActiveRecord::Associations::CollectionProxy)
+          record = if reflection.scope
+                     through_record.association(reflection.source_reflection_name).scope.merge(reflection.scope)
+                   else
+                     through_record.send(reflection.source_reflection_name)
+                   end
+          record = record.first if record.is_a?(::ActiveRecord::Relation)
           return nil unless record
 
           record.tap { |r| set_inverse_instance(r) }
-        rescue StandardError # If scope depend on other table, will raise exception
+        rescue StandardError
           super
         end
       end
