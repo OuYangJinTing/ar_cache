231cfd35c2c197bf41628f4f914ba39fb8debd81
OuYangJinTing <2729877005qq@gmail.com>
2021-03-09 17:12:31 +0800

    Simplify has_one through cache implementation

--- lib/ar_cache/active_record/associations/has_one_through_association.rb
+++ lib/ar_cache/active_record/associations/has_one_through_association.rb
@@ -16,20 +16,10 @@ module ArCache
             Base.strict_loading_violation!(owner: owner.class, association: reflection.name)
           end
 
-          # TODO: Should not instantiate AR
-          through_record = owner.send(reflection.through_reflection.name)
-          through_record = through_record.first if through_record.is_a?(::ActiveRecord::Relation)
-          return nil if through_record.nil? || through_record.destroyed?
+          ::ActiveRecord::Associations::Preloader.new.preload(owner, reflection.name)
 
-          record = if reflection.scope
-                     through_record.association(reflection.source_reflection_name).scope.merge(reflection.scope)
-                   else
-                     through_record.send(reflection.source_reflection_name)
-                   end
-          record = record.first if record.is_a?(::ActiveRecord::Relation)
-          return nil unless record
-
-          record.tap { |r| set_inverse_instance(r) }
+          through_recoed = owner.send(reflection.through_reflection.name)
+          through_recoed.nil? || through_recoed.destroyed? ? nil : owner.send(reflection.name)
         rescue StandardError
           super
         end
